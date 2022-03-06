7699790fe773b09da9f02c712e3b59e891c85627
Ethan <ethanstillwell00@gmail.com>
2022-03-06 16:30:42 -0600

    Update Main.java

--- main/java/com/example/capstone/Main.java
+++ main/java/com/example/capstone/Main.java
@@ -6,8 +6,11 @@
 package com.example.capstone;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -18,6 +21,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
@@ -25,11 +29,91 @@ import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private Stage stage;
 
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        stage = primaryStage;
+        Scene scene = logInScene();
+        Image icon = new Image("C:\\Users\\ethan\\IdeaProjects\\CapStone\\src\\icon.png");
+        primaryStage.getIcons().add(icon);
+        primaryStage.setTitle("TutorU");
+        primaryStage.setWidth(1280.0D);
+        primaryStage.setHeight(720.0D);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-    public void start(Stage primaryStage) throws IOException {
+    public Scene logInScene() throws FileNotFoundException {
+        StackPane root = new StackPane();
+
+        Button login = new Button("-->");
+
+        login.setOnMouseEntered((e) -> {
+            login.setStyle("-fx-background-color: #9c8867; -fx-font-size: 2em; -fx-text-fill: #ffffff");
+        });
+        login.setOnMouseExited((e) -> {
+            login.setStyle("-fx-background-color: #948c7e; -fx-font-size: 2em; -fx-text-fill: #ffffff");
+        });
+        login.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                stage.setScene(HomeLobbyScene());
+            }
+        });
+
+        login.setStyle("-fx-background-color: #948c7e; -fx-font-size: 2em; -fx-text-fill: #ffffff");
+        Shape wSide = new Rectangle(350.0D, 720.0D, Paint.valueOf("ffffff"));
+        TextField userText = new TextField();
+        userText.setMaxWidth(150.0D);
+        PasswordField pwdText = new PasswordField();
+        pwdText.setMaxWidth(150.0D);
+        Label userName = new Label("Username:");
+        Label pw = new Label("Password:");
+        Label menuTitle = new Label("TutorU", new ImageView(new Image(new FileInputStream("C:\\Users\\ethan\\IdeaProjects\\CapStone\\src\\icon2.png"))));
+        menuTitle.setStyle("-fx-background-color: rgba(0, 100, 100, 0.0); -fx-font-size: 2em; -fx-text-fill: #000000");
+        StackPane.setAlignment(menuTitle, Pos.TOP_LEFT);
+        StackPane.setAlignment(login, Pos.CENTER_LEFT);
+        StackPane.setAlignment(userText, Pos.CENTER_LEFT);
+        StackPane.setAlignment(pwdText, Pos.CENTER_LEFT);
+        StackPane.setAlignment(userName, Pos.CENTER_LEFT);
+        StackPane.setAlignment(pw, Pos.CENTER_LEFT);
+        StackPane.setAlignment(wSide, Pos.CENTER_LEFT);
+        menuTitle.setTranslateX(100.0D);
+        userText.setTranslateX(100.0D);
+        userText.setTranslateY(-140.0D);
+        userName.setTranslateX(100.0D);
+        userName.setTranslateY(-160.0D);
+        pwdText.setTranslateX(100.0D);
+        pwdText.setTranslateY(-90.0D);
+        pw.setTranslateX(100.0D);
+        pw.setTranslateY(-110.0D);
+        login.setTranslateX(140.0D);
+        login.setTranslateY(50.0D);
+        root.getChildren().addAll(new Node[]{new ImageView("C:\\Users\\ethan\\IdeaProjects\\CapStone\\src\\MainB.png"), wSide, login, userText, pwdText, pw, userName, menuTitle});
+
+        /*Image icon = new Image("C:\\Users\\ethan\\IdeaProjects\\CapStone\\src\\icon.png");
+        primaryStage.getIcons().add(icon);
+
+        //customizing size of the stage
+        primaryStage.setTitle("TutorU");
+        primaryStage.setWidth(1280.0D);
+        primaryStage.setHeight(720.0D);*/
+
+        return new Scene(root);
+    }
+
+    protected Scene HomeLobbyScene(){
+        StackPane root = new StackPane();
+        Label userName = new Label("Username:");
+        root.getChildren().add(userName);
+        return new Scene(root);
+    }
+
+    /*public void startt(Stage primaryStage) throws IOException {
         StackPane root = new StackPane();
         Button login = new Button("-->");
+
         login.setOnMouseEntered((e) -> {
             login.setStyle("-fx-background-color: #9c8867; -fx-font-size: 2em; -fx-text-fill: #ffffff");
         });
@@ -68,15 +152,23 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         Image icon = new Image("C:\\Users\\ethan\\IdeaProjects\\CapStone\\src\\icon.png");
         primaryStage.getIcons().add(icon);
+
+        //customizing size of the stage
         primaryStage.setTitle("TutorU");
         primaryStage.setWidth(1280.0D);
         primaryStage.setHeight(720.0D);
         primaryStage.setResizable(false);
+        //full screen options
+        primaryStage.setFullScreen(false);
         primaryStage.setFullScreenExitHint("ESCAPE BY PRESSING q");
         primaryStage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+
+
+
+        //setting the scene of the stage and presenting it
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
+    }*/
 
     public static void main(String[] args) {
         Application.launch(args);
