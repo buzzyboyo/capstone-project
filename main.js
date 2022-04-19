1a4f8d8e8b7275abced6f1a154602d11df3d05e4
Ethan <ethanstillwell00@gmail.com>
2022-04-19 09:39:09 -0500

    Creating a MenuTemplate
    Created a Label "File" with a Quit option to start with.

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
-const {app, BrowserWindow} = require("electron");
+const electron = require('electron');
+
+const url = require('url');
+const path = require('path');
+//const { electron } = require("process");
+
+const {app, BrowserWindow, Menu} = electron;
 
 let win = null;
 
-const createWindow = () => {
+app.on('ready', function(){
+    //creatre new window
+    win = new BrowserWindow({});
+    //load html into window
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol:'file',
+        slashes: true
+    }));
+
+    //build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+    //insert menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const mainMenuTemplate = [
+    {
+    label: 'File',
+    submenu:[
+        {
+            label: 'Quit',
+            click(){
+                app.quit();
+            }
+        }
+    ]
+    }
+];
+
+/*const createWindow = () => {
     win = new BrowserWindow({
         width: 1280,
         height: 720,
@@ -15,4 +51,4 @@ const createWindow = () => {
     win.loadFile("index.html")//load html file that will have all of the front end stuff
 };
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createWindow);*/
